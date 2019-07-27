c191027d5cde2dc5270852f272734ad81c153910
Rax Chen <rockpose904@gmail.com>
2019-07-27 13:27:36 +0800

    Add Count

--- guess-number.py
+++ guess-number.py
@@ -2,18 +2,25 @@
 #讓使用者重複輸入數字去猜
 #猜對的話 印出"終於猜對了!"
 #猜錯的話 印出"比答案再大/小"
+#印出猜了幾次
 
 import random
 
 r = random.randint(1,100) #正確答案
+x = 0
 
 while True:
 	i = input ('請輸入1到100其中一個數字: ') #使用者輸入數字
 	i = int(i)
+
 	if i > r:
+		x += 1
 		print('數字再小一點!')
 	elif i < r:
 		print('數字再大一點!')
+		x += 1
 	elif i == r:
-		print('終於猜對了!!')
+		x += 1
+		print('終於猜對了!!您總共猜了', x ,'次。')
 		break
+	print('您猜了', x ,'次。')
